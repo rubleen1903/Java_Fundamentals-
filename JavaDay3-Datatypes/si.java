cfbc8fde44783e04fe94d87f2e0f409886858949
rubleen1903 <rubleen16@gmail.com>
2021-09-06 13:19:12 +0530

    day 3 updated

---
+++ JavaDay3-Datatypes/si.java
@@ -0,0 +1,10 @@
+
+public class si {
+     public static void main(String[] args) {
+        double p = 2500.0, rate = 6.0 , time = 2.0; 
+        double simpleinterest = (p*rate*time)/100;
+        System.out.println("Simple Interest = "+simpleinterest);
+    }
+
+    // si = (P*T*R)/100;
+}
---
+++ JavaDay3-Datatypes/sum.java
@@ -0,0 +1,13 @@
+public class sum {
+    
+    public static void main(String[] args)
+    {   int a = 5;
+        int b = 8;
+        char c ='c';
+        System.out.println(a+c);
+        int s = a +b;
+        System.out.println(s);
+        System.out.print(a+b);
+
+    }
+}
